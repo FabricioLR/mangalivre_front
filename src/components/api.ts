5af0cf4ee809042e9826054e9aff8ecfbefe2e9c
FabricioLR <dimamitoyt@gmail.com>
2023-03-06 18:10:27 -0300

    update url api

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios"
 
+//"http://localhost:4000/"
+//"https://mangalivre-back.onrender.com"
+
 const api = axios.create({
-    baseURL: "http://localhost:4000/"
+    baseURL: "https://mangalivre-back.onrender.com"
 })
 
 export default api
